2bc10aa4e2a67cbc095af201f6bd467623081727
Ahmed refat <69695935+AhmedRefat10@users.noreply.github.com>
2023-09-13 14:19:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def load_data(data):
   else:
     df = pd.read_excel(data, nrows=2000)
   return df
-  
+
+
 # set title and description of the proj
 st.title("ML web app using PyCaret and Streamlit")
 st.write("upload dataset, Perform EDA, select the target var, and choose models for automatic training")
@@ -26,19 +27,20 @@ data = st.file_uploader("upload a CSV or Excel file", type=['csv', 'xlsx'])
 
 if data:
   df = load_data(data)
-  
+  st.subheader('EDA')
   st.write('uploaded dataset:')
   st.write(df.head())
 
-  # EDA plots
-  st.subheader('EDA')
-      
+  #1️⃣EDA
   if st.checkbox("shape"): 
     st.write(df.shape)
 
   if st.checkbox("show values counts") : 
     st.write(df.iloc[:, -1].value_counts())
-              
+  
+  if st.checkbox('show null sum'):
+    st.write(df.isna().sum())
+  
   if st.checkbox("description"): 
     st.write(df.describe())
             
@@ -58,8 +60,8 @@ if data:
     st.write(sns.heatmap(df.corr(),annot=True))
     st.pyplot(fig)
     plt.close(fig)
-    
-  # data visualization
+
+  # data Visualization
   st.subheader('Data visualization')        
   cols_names = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
       
@@ -84,31 +86,56 @@ if data:
     df[selected_cols].plot(kind=plot_type, ax=ax)
     st.pyplot(fig)
   
-  # Ml Settings
-  st.subheader("ML settings")
-  target_var = st.selectbox("select target var", df.columns)
-  supervised_model = st.selectbox('select model', ['none','regression', 'classification'])
-  if supervised_model != 'none':
-    setup_data = setup(df, target=target_var, session_id=123)
+  #2️⃣ preprocessing
+  st.subheader('Preprocessing')
   
-    # model Training
+  col_to_drop = st.multiselect("what cols you want to drop", df.columns)
+  
+  df = df.drop(col_to_drop, axis=1)
+  if st.checkbox("show columns after drop"): 
+    st.write(df.columns.tolist())
+  
+  # fill nan in numerical
+  num_cols = df.select_dtypes(include=['int64', 'float64']).columns
+  for col in num_cols:
+    if df[col].isnull().any():
+      st.markdown(f'**handling null values in {col}**')
+      num_na_type = st.selectbox(f'select how to handle null values in {col}', ['none', 'mean', 'median', 'mode'])
+      if num_na_type == 'mean':
+        df[col].fillna(df[col].mean(), inplace=True)
+      elif num_na_type == 'median':
+        df[col].fillna(df[col].median(), inplace=True)
+      elif num_na_type == 'mode':
+        mode_val = df[col].mode()[0]
+        df[col].fillna(mode_val, inplace=True)
+        
+  # fill nan in categorical
+  cat_cols = df.select_dtypes(include=['object']).columns
+  for col in cat_cols:
+    if df[col].isnull().any():
+      st.markdown(f'**handling null values in {col}**')
+      cat_na_type = st.selectbox(f"select how to handle null values in {col}", ['none','most freq', 'add missing class'])
+      if cat_na_type == 'most freq':
+        most_freq_val = df[col].mode()[0]
+        df[col].fillna(most_freq_val, inplace=True)
+      elif cat_na_type == 'add missing class':
+        df[col].fillna('Missing', inplace=True)
+  
+  if st.button('show data info after processing'):
+    st.write(df.head())
+    st.write("columns after fill null values")
+    st.write(df.isna().sum())
     
-    if supervised_model == 'classification':
+  #3️⃣ ML settings
+  st.subheader('ML settings')
+  target_var = st.selectbox("select target variable", df.columns)
+  if df[target_var].dtype in ['int64', 'float64']:
+    task_type = 'regression'
+  else:
+    task_type = 'classification'
+  if st.button('train model'):
+    with st.spinner('training models...'):
+      setup_data = setup(df, target=target_var, session_id=123)
       models = compare_models()
-    else:
-      models = compare_models(fold=5, round=2, sort="MAE")
-    
-    # evaluate model
-    evaluate_model(models)
-    
-    # get score
-    st.subheader('Model performance')
-    test_set = df.copy()
-    test_set.drop(target_var, axis=1, inplace=True)
-    
-    predictions = predict_model(models, data=test_set)
-    if supervised_model == 'classification':
-      score = accuracy_score(df[target_var], predictions['prediction_label'])
-    else:
-      score = mean_squared_error(df[target_var], predictions['prediction_label'])
-    st.write(f'accuracy : {score:.2f}')
+    st.success("models trained")
+    st.write(pull().style.highlight_max(axis=0))
